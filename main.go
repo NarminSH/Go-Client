bbe9b8ccb914d22befe7e10a50e9c8c50f0de618
Narmin <narmin@Narmins-MacBook-Air.local>
2021-11-01 13:11:22 +0400

    docker change

--- main.go
+++ main.go
@@ -18,46 +18,41 @@ import (
 var db *gorm.DB
 var err error
 
-
-type Client struct{
-	ID           uint     `json:"id" gorm:"primary_key"` //properties
-	FirstName    string   `json:"firstname" gorm:"type:varchar(50)"`
-	LastName     string   `json:"lastname" gorm:"type:varchar(50)"`
-	Patronymic   string   `json:"patronymic" gorm:"type:varchar(70)"`
-	Username     string   `json:"username" gorm:"type:varchar(70)"`
-	PhoneNumber  string   `json:"phone_number" gorm:"type:varchar(70)"`
-	Email       string    `json:"email" gorm:"type:varchar(100)"`
-	CreatedAt    time.Time 
-  	UpdatedAt    time.Time
-
+type Client struct {
+	ID          uint   `json:"id" gorm:"primary_key"` //properties
+	FirstName   string `json:"firstname" gorm:"type:varchar(50)"`
+	LastName    string `json:"lastname" gorm:"type:varchar(50)"`
+	Patronymic  string `json:"patronymic" gorm:"type:varchar(70)"`
+	Username    string `json:"username" gorm:"type:varchar(70)"`
+	PhoneNumber string `json:"phone_number" gorm:"type:varchar(70)"`
+	Email       string `json:"email" gorm:"type:varchar(100)"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
-
-
-
 
 //slice named clients inherits from Client struct
 var clients []Client
 
-
-func getCLients(w http.ResponseWriter, r *http.Request){
+func getCLients(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	db.Find(&clients)
 	json.NewEncoder(w).Encode(clients)
 
 }
 
-func getClient(w http.ResponseWriter, r *http.Request){
+func getClient(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	id := params["id"]
-	var client Client 
+	var client Client
 	db.First(&client, id)
 	json.NewEncoder(w).Encode(client)
 }
-// http.ResponseWriter -  contains the response details(headers, payload) 
+
+// http.ResponseWriter -  contains the response details(headers, payload)
 // http.Request -  contains the incoming request details
 
-func createClient(w http.ResponseWriter, r *http.Request){
+func createClient(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("worked create")
 	w.Header().Set("Content-Type", "application/json")
 	var newClient Client
@@ -68,8 +63,7 @@ func createClient(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode((newClient))
 }
 
-
-func updateClient(w http.ResponseWriter, r *http.Request){
+func updateClient(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	requestId := params["id"]
@@ -77,21 +71,18 @@ func updateClient(w http.ResponseWriter, r *http.Request){
 	var updatedclient Client
 	json.NewDecoder(r.Body).Decode(&updatedclient)
 	fmt.Println(clients, "clientsss")
-	for i, item := range clients{
-		if item.ID == uint(id64){
+	for i, item := range clients {
+		if item.ID == uint(id64) {
 			fmt.Println(i, "iiiii")
-			updatedclient.ID = uint(id64)  //set id from params, same id as previous
+			updatedclient.ID = uint(id64) //set id from params, same id as previous
 			db.Save(updatedclient)
 			json.NewEncoder(w).Encode(updatedclient)
 			return
-		} 
+		}
 	}
 }
 
-
-
-
-func deleteClient(w http.ResponseWriter, r *http.Request){
+func deleteClient(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	requestId := params["id"]
@@ -101,28 +92,24 @@ func deleteClient(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 }
 
+func main() {
+	db, err = gorm.Open("postgres", "host=client_postgres_1 user=lezzetly password=lezzetly123 dbname=db_name port=5432 sslmode=disable Timezone=Asia/Baku")
 
-func main(){
-	db, err = gorm.Open("postgres", "user=lezzetly password=lezzetly123 dbname=db_name port=5432 sslmode=disable")
-
-	if err!=nil{
+	if err != nil {
 		fmt.Println(err, "error is  here")
 		log.Println("Connection Failed to Open")
-	}else{ 
+	} else {
 		log.Println("Connection Established")
 	}
-	
+
 	clients = append(clients, Client{ID: 1, FirstName: "Nermin", LastName: "Chicken wings", Patronymic: "Khan"},
-	Client{ID: 2, FirstName: "Fuad", LastName: "Yarpaq dolmasi", Patronymic: "Abdulla"})
+		Client{ID: 2, FirstName: "Fuad", LastName: "Yarpaq dolmasi", Patronymic: "Abdulla"})
 
 	// Create the database. This is a one-time step.
-    // Comment out if running multiple times - You may see an error otherwise
+	// Comment out if running multiple times - You may see an error otherwise
 	// db.Exec("CREATE DATABASE client_db")
-    // db.Exec("USE client_db")
+	// db.Exec("USE client_db")
 	db.AutoMigrate(&Client{})
-	
-
-
 
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1.0/clients", getCLients).Methods("GET")
