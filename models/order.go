2e0a72dff5fb7591630c5cfc218a88e91eda8e9e
Narmin <narmin@Narmins-MacBook-Air.local>
2021-11-11 11:59:52 +0400

    customer info removed

--- models/order.go
+++ models/order.go
@@ -16,11 +16,6 @@ type Order struct {
 	CookId                int64          `json:"cook_id" gorm:"index"`
 	CourierId             int64         `json:"courier" gorm:"foreignKey:OrderId"`
 	DeliveryInformationId  int64        `json:"delivery_information_id" gorm:"foreignKey:"`
-	CustomerFirstName      string       `json:"customer_first_name" gorm:"type:varchar(150); not null"`
-	CustomerLastName      string        `json:"customer_last_name" gorm:"type:varchar(150); not null"`
-	CustomerPhone         string        `json:"customer_phone" gorm:"type:varchar(12); not null"`
-	CustomerLocation      string        `json:"customer_location" gorm:"type:varchar(150); not null"`
-	CustomerEmail        string         `json:"customer_email" gorm:"type:varchar(254); not null"`
 	Complete             bool           `json:"complete"`
 	IsRejected           bool           `json:"is_rejected"`
 	RejectReason         string         `json:"reject_reason" gorm:"type:varchar(250)"`
