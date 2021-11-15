2fa5997a31b65e4aa1e8190c92f98ec7458aa971
Narmin <narmin@Narmins-MacBook-Air.local>
2021-11-15 15:11:34 +0400

    host

--- main.go
+++ main.go
@@ -318,7 +318,7 @@ func main() {
 	db.AutoMigrate(&models.Client{})
 	// db.AutoMigrate(&models.Order{})
 	// db.AutoMigrate(&models.Item{})
-	db.Model(&models.Order{}).AddForeignKey("client_id", "clients(id)", "NO ACTION", "NO ACTION")
+	// db.Model(&models.Order{}).AddForeignKey("client_id", "clients(id)", "NO ACTION", "NO ACTION")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1.0/clients", getCLients).Methods("GET")
