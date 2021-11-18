ded6f79fb2a3ef8dc69b7662671fb22c430d272e
Narmin <narmin@Narmins-MacBook-Air.local>
2021-11-18 17:51:41 +0400

    item preload

--- main.go
+++ main.go
@@ -380,7 +380,7 @@ func clientActiveOrders(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(user, "user over herereee")
 	fmt.Println(client.ID, "client id is over here")
 	client_id := client.ID
-	var orders models.Order
+	var orders []models.Order
 	a := db.Where("client_id = ? AND complete = ? ", client_id, "False").Preload("Items").Find(&orders)
 	fmt.Println(a, "database answerrr")
 	// db.Preload("Items").First(&orders, "client_id = ? AND complete = ? ", client_id, "False")
